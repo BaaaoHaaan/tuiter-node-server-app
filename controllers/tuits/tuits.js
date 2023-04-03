783168d3e9e3a176a3d2166de7e938ec588704f3
Han Bao <baaaohaaan@outlook.com>
2023-04-03 15:40:46 -0700

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    "_id": 123,
+    "topic": "Space",
+    "headshot": "/elon-musk.png",
+    "userName": "Elon Musk",
+    "handle": "@elonmusk",
+    "time": "23h",
+    "title": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
+    "tuit": "Amazing show about @Inspiration4x mission!",
+    "image": "/space-man.jpg",
+    "link": "netflix.com",
+    "linkHeading": "Countdown: Inspiration4 Mission to Space | Netflix Official Site",
+    "linkSummary": "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space...",
+    "liked": true,
+    "replies": 123,
+    "retuits": 432,
+    "likes": 2345
+  },
+  {
+    "_id": 234,
+    "topic": "People",
+    "headshot": "/new-york-post.png",
+    "userName": "New York Post",
+    "handle": "@nypost",
+    "time": "23h",
+    "title": "Karl Marx and Elon Musk",
+    "tuit": "Grimes seen reading Karl Marx following split with world's richest man Elon Musk",
+    "image": "",
+    "link": "",
+    "linkHeading": "",
+    "linkSummary": "",
+    "liked": false,
+    "replies": 965,
+    "retuits": 2400,
+    "likes": 4000
+  }
+];
